8e08c5603f644508897816ea0af2c7e0ec03696a
Anna Laura <nalauramoura@gmail.com>
2025-04-26 09:15:55 -0300

    update

--- script.sh
+++ script.sh
@@ -3,43 +3,47 @@
 nano iacl.sh
 /bin/bash
 
-echo "Criando diretórios..."
+echo "Criando diretórios"
 
-mkdir /publico
-mkdir /adm
-mkdir /ven
-mkdir /sec
+mkdir -p /publico /adm /ven /sec
 
-echo "Criando grupos..."
+echo "Criando grupos"
 
-groupadd GRP_ADM
-groupadd GRP_VEN
-groupadd GRP_SEC
+#Para verificar se o grupo existe antes de criar, adicione:
 
-echo "Criando usuários..."
+getent group nome-grupo
 
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd romeu -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+groupadd GRUPO_ADM
+groupadd GRUPO_SEC
+groupadd GRUPO_VEN
 
-useradd olivia -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd aurora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd raul -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+echo "Criando usuários"
 
-useradd diana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd theo -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd laura -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+#Para verificar se o usuário existe antes de criar, adicione:
 
+id -u nome-usuario &>/dev/null 
 
-echo "Especificando permissões dos diretórios.."
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRUPO_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRUPO_ADM
+useradd romeu -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRUPO_ADM
 
-chown root:GRP_ADM /adm
-chown root:GRP_VEN /ven
-chown root:GRP_SEC /sec
+useradd olivia -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRUPO_VEN
+useradd aurora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRUPO_VEN
+useradd raul -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRUPO_VEN
+
+useradd diana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRUPO_SEC
+useradd theo -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRUPO_SEC
+useradd laura -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRUPO_SEC
+
+echo "Especificando permissões dos diretórios"
+
+chown root:GRUPO_ADM /adm
+chown root:GRUPO_VEN /ven
+chown root:GRUPO_SEC /sec
 
 chmod 770 /adm
-chmod 770 /ven
-chmod 770 /sec
 chmod 777 /publico
+chmod 770 /sec
+chmod 770 /ven
 
 echo "Fim do projeto!"
